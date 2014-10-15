ffb5be4b8d37f1b2dd2b99c62ee3c7d706d74f6f
Ruth Hutchins <ruthdhan@gmail.com>
2014-10-14 17:30:41 -0700

    Add a gulp configuration to start the server using nodemon.  Start browsersync from express itself; otherwise browser-sync gets confused when nodemon restarts the express server.

--- tessr.js
+++ tessr.js
@@ -3,6 +3,11 @@
 // Requires:
 var express = require('express');
 var routes = require('./routes');
+var browserSync = require('browser-sync');
+
+// Constants
+var ENV = process.env.NODE_ENV || 'development';
+var PORT = 3000;
 
 // Use express and export it as well.
 var app = module.exports = express();
@@ -10,4 +15,16 @@ var app = module.exports = express();
 // Set up routing for express.
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(PORT, function() {
+  console.log('Listening on port ' + PORT);
+
+  // Since the server is up, start a browser-sync instance for development
+  // purposes.
+  if (ENV === 'development') {
+    browserSync({
+      proxy: 'localhost:' + PORT,
+      files: ['routes/*.js'],
+      port: 8080
+    });
+  }
+});
