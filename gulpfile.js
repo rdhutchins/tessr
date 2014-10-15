c78dca6052ff7fbb28619809ca7b21a0e98104e8
Ruth Hutchins <ruthdhan@gmail.com>
2014-10-15 00:48:54 -0700

    Switch to using jade templates and fix the browsersync setup to reload automatically

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,48 @@
 // gulpfile.js
 
 var gulp = require('gulp');
-var nodemon = require('nodemon');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');
 
-gulp.task('server', function() {
-  nodemon({ script: 'tessr.js' });
+// Configuration:
+var kPaths = {
+  VIEWS: 'views/*.jade',
+  ROUTES: 'routes/*.js',
+  SERVER: 'tessr.js'
+};
+
+// server:
+//   Start tessr server using nodemon to watch for changes and restart as
+//   necessary.  Pass restarts on to browserSync.
+gulp.task('server', function(done) {
+  // nodemon will re-run the given script when the paths under watch change.
+  // By default it only watches the current directory.  Add also any backend
+  // paths here.  NOTE: template paths should NOT be added here as they do not
+  // require an express server restart.
+  nodemon({
+    script: 'tessr.js',
+    watch: [kPaths.ROUTES, kPaths.SERVER]
+  }).on('start', handleStart);
+
+  function handleStart() {
+    if (!browserSync.active) {
+      browserSync({
+        proxy: 'localhost:3000',
+        port: 5000,
+      }, done);
+    } else {
+      console.log('Server reloaded but I cannot automatically reload');
+    }
+  }
 });
 
-gulp.task('default', ['server']);
+// watch:
+//   Watch paths of interest not already handled by nodemon and reload
+//   browser-sync when they change.
+gulp.task('watch', function() {
+  gulp.watch(kPaths.VIEWS, browserSync.reload);
+});
+
+
+// By default: start the tessr server and watch for changes.
+gulp.task('default', ['server', 'watch']);
--- tessr.js
+++ tessr.js
@@ -2,29 +2,24 @@
 
 // Requires:
 var express = require('express');
+var path = require('path');
 var routes = require('./routes');
-var browserSync = require('browser-sync');
 
 // Constants
 var ENV = process.env.NODE_ENV || 'development';
 var PORT = 3000;
+var VIEWS_DIR = path.join(__dirname, 'views');
 
 // Use express and export it as well.
 var app = module.exports = express();
+
+// Settings for express.
+app.set('views', VIEWS_DIR);
+app.set('view engine', 'jade');
 
 // Set up routing for express.
 app.get('/', routes.index);
 
 app.listen(PORT, function() {
   console.log('Listening on port ' + PORT);
-
-  // Since the server is up, start a browser-sync instance for development
-  // purposes.
-  if (ENV === 'development') {
-    browserSync({
-      proxy: 'localhost:' + PORT,
-      files: ['routes/*.js'],
-      port: 8080
-    });
-  }
 });
